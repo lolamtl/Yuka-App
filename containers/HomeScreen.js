e42cf9cce8890adf2f9b77b20091ff28a5a2c4d6
Lola <lmonteil@live.fr>
2021-04-12 17:49:02 +0200

    change

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigation, useRoute } from "@react-navigation/core";
+import { useNavigation } from "@react-navigation/core";
 import { FontAwesome } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-community/async-storage";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
